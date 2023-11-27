024627eb4fc3fec07c5e963f4780e5624d62e1af
lucasackerberg <lucas@ackerberg.se>
2023-11-27 21:26:00 +0100

    added select fetch urls
    added select fetch urls and created divs in js

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
+const selectedGenre = document.getElementById('gameGenres');
+selectedGenre_text = selectedGenre.value;
+const selectedPlatform = document.getElementById('platform');
+selectedPlatform_text = selectedPlatform.value;
+
+selectedGenre.addEventListener('change', handleGenreChange);
+selectedPlatform.addEventListener('change', handlePlatformChange);
+
 const url = 'https://mmo-games.p.rapidapi.com/games';
-const options = {
+const options = {   
     method: 'GET',
     headers: {
         'X-RapidAPI-Key': '55ef4021bamshd539b51b15b1807p193c2bjsn1f7f64b08750',
@@ -7,11 +15,32 @@ const options = {
     }
 };
 
+const getMMOListByPlatformAndCategory = async (selectedPlatform_text, selectedGenre_text) => {
+    const url = `https://mmo-games.p.rapidapi.com/games?platform=${selectedPlatform_text}&category=${selectedGenre_text}`;
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '55ef4021bamshd539b51b15b1807p193c2bjsn1f7f64b08750',
+            'X-RapidAPI-Host': 'mmo-games.p.rapidapi.com'
+        }
+    };
+
+    try {
+        const response = await fetch(url, options);
+        const CatMmoData = await response.json();
+        WriteOutGamesByPlatformAndGenre(CatMmoData);
+        console.log(CatMmoData);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
 const getMMOlist = async () => {
     try {
         const response = await fetch(url, options);
         const mmoData = await response.json();
         writeOut(mmoData);
+        console.log(mmoData);
     } catch (error) {
         console.error(error);
     }
@@ -42,5 +71,35 @@ const writeOut = (mmoData) => {
         gamesListTwo.appendChild(gameElement);
     });
 }
-
 getMMOlist();
+
+getMMOListByPlatformAndCategory();
+
+const WriteOutGamesByPlatformAndGenre = (CatMmoData) => {
+    const smallerGameWrapper = document.getElementById('randomCWrapper');
+    CatMmoData.forEach((catGame) => {
+        const smallerGame = document.createElement('div');
+        smallerGame.classList.add('smallerGame');
+        smallerGame.innerHTML = `<img src="${catGame.thumbnail}" alt="A picture of the game"> <h3>${catGame.title}</h3> <p>${catGame.short_description}</p> <p>Release Date: ${catGame.release_date}</p>`;
+        smallerGameWrapper.appendChild(smallerGame);
+    });
+}
+
+function handleGenreChange() {
+    selectedGenre_text = selectedGenre.value;
+    clearSmallerGameList();
+    getMMOListByPlatformAndCategory(selectedPlatform_text, selectedGenre_text);
+    console.log(selectedGenre_text);
+}
+
+function handlePlatformChange() {
+    selectedPlatform_text = selectedPlatform.value;
+    clearSmallerGameList();
+    getMMOListByPlatformAndCategory(selectedPlatform_text, selectedGenre_text);
+    console.log(selectedPlatform_text);
+}
+
+function clearSmallerGameList() {
+    const clearWrapper = document.getElementById('randomCWrapper');
+    clearWrapper.innerHTML = '';
+}
